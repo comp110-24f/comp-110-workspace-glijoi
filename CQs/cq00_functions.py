681fce027577c9cbf3c611405da985a7537d7807
glijoi <glijoi@ad.unc.edu>
2024-08-28 12:33:39 -0400

    Finished CQ

--- CQs/cq00_functions.py
+++ CQs/cq00_functions.py
@@ -1,1 +1,17 @@
 """functions challenge question"""
+
+__author__ = "730667690"
+
+
+def mimic(message: str) -> str:
+    """returns message back"""
+    return message
+
+
+def main() -> None:
+    """print result of calling mimic"""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
