7265ce23b6167d2cbe40a25875fe2265dc749362
glijoi <glijoi@ad.unc.edu>
2024-10-14 14:45:42 -0400

    finished cq07

--- CQs/cq07/find_max.py
+++ CQs/cq07/find_max.py
@@ -4,7 +4,20 @@ __author__ = "730667690"
 
 
 def find_and_remove_max(vals: list[int]) -> int:
-    """find and return largest number in list"""
-    if len(vals) = 0:
+    """find and return largest # in list AND remove all instances of largest #"""
+    if len(vals) == 0:
         return -1
     else:
+        big: int = 0
+        idx: int = 0
+        while idx < len(vals):
+            if vals[idx] > big:
+                big = vals[idx]
+            idx += 1
+        idx = 0
+        while idx < len(vals):
+            if vals[idx] == big:
+                vals.pop(idx)
+            else:
+                idx += 1
+        return big
--- CQs/cq07/max_test.py
+++ CQs/cq07/max_test.py
@@ -1,3 +1,25 @@
-""""""
+"""Test functions of find_max"""
 
 __author__ = "730667690"
+
+from CQs.cq07.find_max import find_and_remove_max
+
+
+def test_find_and_remove_max_return() -> None:
+    """find_and_remove_max should return biggest number in list"""
+    vals = [10, 9, 8, 7, 10]
+    assert find_and_remove_max(vals) == 10
+
+
+def test_find_and_remove_max_mutate() -> None:
+    """find_and_remove_max should remove all instances of biggest number from list"""
+    vals = [10, 9, 8, 7, 10]
+    find_and_remove_max(vals)
+    assert vals == [9, 8, 7]
+
+
+def test_find_and_remove_max_edge() -> None:
+    """find_and_remove_max should return correct value if unconventional input"""
+    vals = [1, 1, 1]
+    find_and_remove_max(vals)
+    assert vals == []
