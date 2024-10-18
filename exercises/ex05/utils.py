3cc00035e0b58edea95fd0f4e326833dcc484cf0
glijoi <glijoi@ad.unc.edu>
2024-10-18 14:47:52 -0400

    ex05 progress

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,46 @@
+"""list utility functions"""
+
+__author__ = "730667690"
+
+
+def only_evens(vals: list[int]) -> list[int]:
+    """returns a list of even numbers from the input list"""
+    idx: int = 0
+    evens: list[int] = []
+    while idx < len(vals):
+        if vals[idx] % 2 == 0:
+            evens.append(vals[idx])
+            idx += 1
+        else:
+            idx += 1
+    return evens
+
+
+def sub(vals: list[int], start: int, end: int) -> list[int]:
+    """returns a list that is a subset of the input list"""
+    idx: int = 0
+    subset: list[int] = []
+    if start < 0:
+        start = 0
+    if end >= len(vals):
+        end = len(vals)  # I initially added a -1 to the end but realized
+        # this was causing errors
+    while idx < len(vals):
+        while idx < start:
+            idx += 1
+        if idx >= end:  # I had an error due to not including an =
+            # I still had errors after, but that was
+            # due to the issue in the previous comment
+            return subset
+        else:
+            subset.append(vals[idx])
+            idx += 1
+    return subset
+
+
+def add_at_index(vals: list[int], elem: int, index: int) -> None:
+    """modifies input list to put elem at index"""
+    if index < 0 or index > len(vals):
+        raise IndexError("Index is out of bounds for the input list")
+    else:
+        vals[index] = elem
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,9 @@
+"""tests for list utility functions"""
+
+__author__ = "730667690"
+
+from exercises.ex05.utils import only_evens, sub, add_at_index
+
+
+def test_only_evens() -> None:
+    """"""
