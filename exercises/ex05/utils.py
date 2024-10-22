5ab919e50a8069664451fa411a143b22315b9051
glijoi <glijoi@ad.unc.edu>
2024-10-22 18:08:42 -0400

    fixed some errors hopefully

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -42,5 +42,9 @@ def add_at_index(vals: list[int], elem: int, index: int) -> None:
     """modifies input list to put elem at index"""
     if (index < 0) or (index > len(vals)):
         raise IndexError("Index is out of bounds for the input list")
+    elif (index == 0) and (len(vals) == 0):
+        vals.append(elem)
+    elif index == len(vals) + 1:
+        vals.append(elem)
     else:
         vals[index] = elem
